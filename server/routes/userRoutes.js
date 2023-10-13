5d1aaf992f98b060c37368c4219d2400027e8ab4
Akinola Adewole <adewoleakinola24@gmail.com>
2023-10-13 23:31:59 +0100

    Wealth Guard

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,6 +6,19 @@ import {
 
 import { protect } from '../middleware/authMiddleware.js.js'
 
-router.post("/",(req,res)=>{
+router.get("/",(req,res)=>{
     res.send(" Server is ready")
 })
+
+router.post("/signup", signup);
+router.post("signin", signin);
+
+router.post("/dashboard", getDashboard);
+router.post("/getwallets", getWallets);
+router.post("/deleter", deleteWallet);
+
+router.post("/transfer", transfer);
+router.post("/updatebalance", updateBalance);
+router.post("/getTransactions", getTransactions);
+
+export default router;
