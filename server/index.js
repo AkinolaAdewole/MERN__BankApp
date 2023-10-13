a68c4450d5f9e595fcb3c4d42d4693e989cac383
Akinola Adewole <adewoleakinola24@gmail.com>
2023-10-13 23:45:35 +0100

    Wealth Guard

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 const app = express()
 
 import cors from 'cors';
 app.use(cors());
 app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
+
+//! UserRoutes
+import userRoutes from "./routes/userRoutes.js"
+
+//! WalletRoutes
+import walletRoutes from "./routes/walletRoutes.js"
+
+app.use('/user', userRoutes);
+app.use('/wallet', walletRoutes);
--- server/routes/walletRoutes.js
+++ server/routes/walletRoutes.js
@@ -1,1 +1,9 @@
 import express from 'express';
+const router = express.Router();
+
+import { createWallet, fundWallet } from '../controllers/WalletController.js';
+
+router.post("/create", createWallet);
+router.post("fundwallet", fundWallet);
+
+export default router
