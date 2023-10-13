93954312a3991947042be587318cb516d4b2e968
Akinola Adewole <adewoleakinola24@gmail.com>
2023-10-13 15:26:45 +0100

    commit

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,9 +1,18 @@
 import userModel from '../models/UserModels.js'
+import generateToken from '../utils/token.js';
 
 const signup = async(req, res) => {
     const form = req.body
-    const newUser = await userModel.findOne({ email });
+    const userExist = await userModel.findOne({ email });
 
+    if(userExist){
+        res.status(400);
+        throw new Error ('User already exist');
+    }
+
+    const newUser = new userModel(form);
+    
+    
   };
 
   
