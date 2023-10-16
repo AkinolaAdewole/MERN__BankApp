0587cd777288fd98e6f1f39dfbdf65818a365660
Akinola Adewole <adewoleakinola24@gmail.com>
2023-10-16 16:21:07 +0100

    Bank App

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Routes></Routes>
 
     </div>
   );
