74b9ece7348c5df4715c45d175a6e83df311cda0
Akinola Adewole <adewoleakinola24@gmail.com>
2023-10-17 23:24:34 +0100

    Bank App

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <>
           <nav className="navbar navbar-expand-lg nav1  sticky-top z-1000 p-0">
-                <div className="d-flex">
+                <div className="container-fluid">
                   <Link className="navbar-brand" to="/">
                     {/* <img src={logo} alt="" width={120} /> */}
                     Akinola
