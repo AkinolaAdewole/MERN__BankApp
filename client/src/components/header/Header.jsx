ae23cd28f83c9759b6762458672a65d9b2795418
Akinola Adewole <adewoleakinola24@gmail.com>
2023-10-18 21:58:22 +0100

    Bank App

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -3,7 +3,40 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+          <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+              </div>
+
+              <div class="carousel-item">
+              </div>
+
+              <div class="carousel-item">
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+
+
+        {/* <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
             <div class="carousel-inner">
                 <div class="carousel-item active">
                   <div>
@@ -35,14 +68,17 @@ const Header = () => {
                 <div class="carousel-item">
                  <div>
                   <div className='col-12 col-md-7 p-5'>
-                    <div className='col-10 mx-auto'></div>
+                    <div className='col-10 mx-auto'>
+                      <h1 className='fw-bold'>
+                        <div className='text-danger'>Safeguard</div>
+                      </h1>
+                    </div>
                   </div>
                  </div>
                 </div>
 
-                {/* <div className='carousel-item'></div> */}
             </div>
-        </div>
+        </div> */}
     </>
   )
 }
