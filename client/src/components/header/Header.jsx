54a0f4c6ca325b0aca1f55a2a3e40a0564063831
Akinola Adewole <adewoleakinola24@gmail.com>
2023-10-18 21:28:24 +0100

    Bank App

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -6,11 +6,24 @@ const Header = () => {
         <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    
+                  <div>
+                    <div className='col-12 col-md-7 p-5'>
+                      <div className='col-10 mx-auto'>
+                        <h1 className='fw-bold'> Banking is personal</h1>
+                        <h1 className='fw-light'> We understand personal</h1>
+                      </div>
+                    </div>
+                  </div>            
                 </div>
 
                 <div class="carousel-item">
-                
+                  <div>
+                    <div className='col-12 col-md-7 p-5'>
+                      <div className='col-10 mx-auto'>
+                        <h1 className='fw-bold'>Deliver a frictionless payment experience that delights customers.</h1>
+                      </div>
+                    </div>
+                  </div>
                 </div>
                 <div class="carousel-item">
                 
