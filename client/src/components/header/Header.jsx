08037641298ba172016aabde9e2bec414c26a54c
Akinola Adewole <adewoleakinola24@gmail.com>
2023-10-18 19:55:31 +0100

    Bank App

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    
+                </div>
+
+                <div class="carousel-item">
+                
+                </div>
+                <div class="carousel-item">
+                
+                </div>
+            </div>
+        </div>
+    </>
   )
 }
 
