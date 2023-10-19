36f8aacdcf897ef95fe09fbb6067764c17608354
Akinola Adewole <adewoleakinola24@gmail.com>
2023-10-19 12:37:06 +0100

    Bank App

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import fraud from '../resources/fraud.jpg'
+import '../header/header.css';
 
 const Header = () => {
   return (
@@ -43,19 +44,18 @@ const Header = () => {
               <div class="carousel-item">
                 <div className="row vh-100 bg-secondary align-items-center">
                   <div className='col-12 col-md-7 p-5'>
-                    <div className='col-10 mx-auto'>
-                        <h1 className='fw-bold'>
-                          <div className='text-danger'>Safeguard</div>
-                          <div className='text-white'> Your Account</div>
-                        </h1>
-                        <p className='text-white'>
-                          We will <strong>NEVER</strong> contact you via phone calls, SMS, or email to request 
-                          your card details, PIN, token codes, mobile/internet banking login details, 
-                          or any other account-related information.
-                        </p>
-                    </div>
                     <div>
-                     <img src={fraud} alt="" />
+                        <div className='col-10 mx-auto'>
+                            <h1 className='fw-bold'>
+                              <div className='text-danger'>Safeguard</div>
+                              <div className='text-white'> Your Account</div>
+                            </h1>
+                            <p className='text-white'>
+                              We will <strong>NEVER</strong> contact you via phone calls, SMS, or email to request 
+                              your card details, PIN, token codes, mobile/internet banking login details, 
+                              or any other account-related information.
+                            </p>
+                        </div>
                     </div>
                   </div>
                   
