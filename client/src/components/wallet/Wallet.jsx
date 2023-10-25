4c334fb69eced8a452be4f6fbcd0a2eace0b31e0
Akinola Adewole <adewoleakinola24@gmail.com>
2023-10-25 13:23:28 +0100

    Bank App clone Application

--- client/src/components/wallet/Wallet.jsx
+++ client/src/components/wallet/Wallet.jsx
@@ -1,9 +1,17 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import chip from '../resources/chip.jpg';
+import visa from "../resources/visa.png";
 
 const Wallet = () => {
   const [message, setMessage] = useState('');
-  const [amout, setAmount] = useState('');
+  const [amount, setAmount] = useState('');
+
+  let id = localStorage.token;
+
+  const fundwallet =()=>{
+    let endpoint = 'http://localhost:4300/wallet/fundwallet'
+  }
   return (
     <div>Wallet</div>
   )
